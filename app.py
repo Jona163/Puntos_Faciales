4b38d0010227cc1d898578c116d12e9edcbaf152
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-27 17:09:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ from flask import Flask, request, render_template, redirect
 import cv2
 import mediapipe as mp
 import pandas as pd
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# Inicializar MediaPipe Face Mesh
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5)
