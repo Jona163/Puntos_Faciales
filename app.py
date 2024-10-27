f4ddd1dc43d04296b6decfb8cfc7bacde8e0adfa
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-27 17:09:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,6 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 # Inicializar MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5)
+
+def sanitize_filename(filename):
+    return re.sub(r'[^a-zA-Z0-9_.-]', '_', filename)
